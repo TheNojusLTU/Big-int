54e6d4d81c203ffa248d538b49fabd8aaec0af73
TheNojusLTU <75728675+TheNojusLTU@users.noreply.github.com>
2026-02-11 19:02:56 +0200

    Added comments

--- bigint.h
+++ bigint.h
@@ -4,8 +4,8 @@
 #include <string.h>
 
 typedef struct {
-    char *value;
-    char sign;
+    char *value; //The array size should be the lenght of the number +1 and the last character '\0'
+    char sign; //If the value of this is not 0 then it is a negative number
     long NumSize;
     void *IsInitialized;
 } BigInt;
